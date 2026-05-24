57fa56243e8347e285017e3ba0229da9ea31d4d9
ajayganuga <ajayganuga@gmail.com>
2026-05-24 22:44:21 +0530

    Day9 Scripts

---
+++ Day9-Scripts/Day9-Scripts.sql
@@ -0,0 +1,32 @@
+-- Q9 --
+
+drop TABLE if exists orders;
+CREATE TABLE orders 
+(
+	customer_id 	INT,
+	dates 			DATE,
+	product_id 		INT
+);
+INSERT INTO orders VALUES
+(1, '2024-02-18', 101),
+(1, '2024-02-18', 102),
+(1, '2024-02-19', 101),
+(1, '2024-02-19', 103),
+(2, '2024-02-18', 104),
+(2, '2024-02-18', 105),
+(2, '2024-02-19', 101),
+(2, '2024-02-19', 106); 
+
+
+select * from orders;
+
+
+-- Solution:
+
+select dates, cast(product_id as varchar) as products 
+from orders
+union
+select dates, string_agg(cast(product_id as varchar),',') as products
+from orders
+group by customer_id, dates
+order by dates, products;
